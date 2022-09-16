0a6d8ebea372b10ff3f9bd3766678ebc17961512
Anderson Oliva <monteirooliva@gmail.com>
2022-09-16 11:00:34 -0300

    first commit with sound stream, weather witgets and dollar quote

---
+++ index.js
@@ -0,0 +1,11 @@
+function playRadio(play) {
+
+    var play = document.querySelector('.stream');
+
+    if (play.style.display === 'none') {
+        play.style.display = 'block'
+    } else {
+        play.style.display = 'block'
+    }
+
+}
